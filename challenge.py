eb9e252b73a96f3e3f3c778ca022f2535326c74f
Tobias <tobias.bosch@student.hu.nl>
2023-03-07 09:59:06 +0100

    didn't mean to delete challenge.py

---
+++ challenge.py
@@ -0,0 +1,12 @@
+# Opdracht: Verzin een unieke banner text en pas dit aan!
+
+def print_banner(text):
+    ''' This function is used to display banner text in the console'''
+    print(f"-{'-'*15}{'-'*len(text)}{'-'*15}-")
+    print(f"-{' '*15}{text}{' '*15}-")
+    print(f"-{'-'*15}{'-'*len(text)}{'-'*15}-")
+
+
+if __name__ == "__main__":
+    print_banner("Wauw kijk nou! Een andere banner voor jou!")
+
